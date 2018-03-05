27b1163c0af38e708e47af0126316b5d08040694
cfdisk <37021153+cfdisk@users.noreply.github.com>
2018-03-05 20:52:52 +0100

    Create spacer.sh

---
+++ spacer.sh
@@ -0,0 +1,31 @@
+!/bin/sh
+
+# scan directory and interactivly delete oldest file while below threshold
+
+THRESHOLD=10000000
+MOVIEDIR=/mnt/hdd/movie/
+
+##############################################################################
+
+checkFree() {
+    sync
+    freeSpace=`df $MOVIEDIR | tail -n 1`
+    freeSpace=`echo $freeSpace | awk -F " " '{print $4}'`
+    echo $freeSpace
+}
+
+getOldestFile() {
+    oldestFile="$(ls -1t $MOVIEDIR | tail -1)"
+    echo $oldestFile
+}
+
+while [ `checkFree` -le "$THRESHOLD" ]; do
+    echo "######################################################"
+    echo "Media ($MOVIEDIR) almost full: Free: `checkFree` / Threshold: $THRESHOLD"
+    echo "Oldest File: $MOVIEDIR`getOldestFile`"
+    rm -i $MOVIEDIR/`getOldestFile`
+    #mv $MOVIEDIR/`getOldestFile` /mnt/hdd/tmp
+    sleep 1
+done
+
+exit 0
